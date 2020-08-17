4dbf25d485ff9fabd105cd9e3c813ea41ba5fb83
Maxim <Maxim13parkour@gmail.com>
2020-08-17 22:32:52 +0300

    Netty Server IO Client withJava FX

--- src/main/java/Server/ClientHandlerReader.java
+++ src/main/java/Server/ClientHandlerReader.java
@@ -6,24 +6,37 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 
+import javax.swing.text.html.ListView;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class ClientHandlerReader extends SimpleChannelInboundHandler<String> {
 
     private AuthList_lite user;
     private String userName;
+    private LinkedList<String> serverList = new LinkedList<>();
+    private Path path;
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("____________________________________");
         System.out.println("Клиент " + ctx.name() + " подключился");
+        File dir = new File("src/main/resources/ServerFiles/max");
+        for (String file : Objects.requireNonNull(dir.list())) {
+            serverList.add(file);
+
+        }
+        ctx.writeAndFlush(serverList);
     }
+
         @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("Клиент отключился");
@@ -60,7 +73,7 @@ public class ClientHandlerReader extends SimpleChannelInboundHandler<String> {
                 //_____________________DOWNLOAD_______________________________
                 System.out.println("Команда от пользователя " + userName + ": " + message);
                 String[] arrFile = message.split(" ");
-                Path path = Paths.get("src/main/resources/ServerFiles/" + userName + "/" + arrFile[1]);
+                Path path = Paths.get("src/main/resources/ServerFiles/" + "max" + "/" + arrFile[1]);
                 FielMessage fileMsg = new FielMessage();
                 FileInputStream input = new FileInputStream(path.toFile());
                 fileMsg.setFileName(path.getFileName().toString());
@@ -80,18 +93,28 @@ public class ClientHandlerReader extends SimpleChannelInboundHandler<String> {
                 System.out.println("Файл " + create.getName(5) + " удалён с сервера");
                 //-----------------------DELETE------------------------------
             } else if (message.startsWith("/upload")) {
-                //-----------------------UPLOAD------------------------------
-                System.out.println("Команда от пользователя " + userName + ": " + message);
+                //-----------------------UPLOAD P1------------------------------
+                System.out.println("Команда от пользователя " + "max" + ": " + message);
                 String[] arrFile = message.split(" ");
-                Path path = Paths.get("src/main/resources/ServerFiles/" + userName + "/" + arrFile[1]);
-                FielMessage file = (FielMessage) msg;
-                if (file instanceof FielMessage) {
-                    Files.createFile(path);
-                    Files.write(path, file.getFileByte(), StandardOpenOption.APPEND);
-                    System.out.println("Готово!");
+                path = Paths.get("src/main/resources/ServerFiles/" + "max" + "/" + arrFile[1]);
+            } else if(message.startsWith("/refresh")) {
+                serverList.clear();
+                File dir = new File("src/main/resources/ServerFiles/max");
+                for (String file : Objects.requireNonNull(dir.list())) {
+                    serverList.add(file);
+
                 }
-                //----------------------UPLOAD---------------------------------
+                ctx.writeAndFlush(serverList);
             }
+                //----------------------UPLOAD P1---------------------------------
+            }
+                //---------------------UPLOAD P2----------------------------------
+        else if (msg instanceof FielMessage) {
+            FielMessage file = (FielMessage) msg;
+            Files.createFile(path);
+            Files.write(path, file.getFileByte(), StandardOpenOption.APPEND);
+            System.out.println("Готово! Файл загружен на сервер");
+            //------------------------UPLOAD P2-----------------------------------
         } else {
             String message = msg.toString();
             System.out.println("Сообщение от пользователя " + userName + ": " + message);
